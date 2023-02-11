a19044f94f9ad444fa8356097268b2352b993fdf
Daniel Krawczyk <daniel6krawczyk@gmail.com>
2023-02-11 10:28:53 +0100

    console.log text changed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("Hejka");
+console.log("Hejka. Ten kod jest już w repozytorium Git");
 
 let section__button = document.querySelector(".section__button");
 let body = document.querySelector(".body");
