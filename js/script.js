9fbd24217c76b2ac74cffc85b69c878d0477c7d7
Daniel Krawczyk <daniel6krawczyk@gmail.com>
2023-03-29 15:22:20 +0200

    js refactoring

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
-console.log("Hejka. Ten kod jest już w repozytorium Git.");
+{
+    const welcome = () => {
+        console.log("Hejka. Ten kod jest już w repozytorium Git.");
+    };
 
-let section__button = document.querySelector(".section__button");
-let body = document.querySelector(".body");
-let section__span = document.querySelector(".section__span");
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const toggleText = document.querySelector(".js-span");
 
-section__button.addEventListener("click", () => {
-    body.classList.toggle("section__button--backgroundSwitch");
+        body.classList.toggle("section__button--toggleBackground");
+        toggleText.innerText = body.classList.contains("section__button--toggleBackground")
+            ? "ciemniejszy" : "jaśniejszy";
+    };
 
-    section__span.innerText = body.classList.contains("section__button--backgroundSwitch")
-        ? "ciemniejszy" : "jaśniejszy";
-});
+    const portraitRemove = () => {
+        const portrait = document.querySelector(".js-portrait");
+        const portraitButton = document.querySelector(".js-portraitRemove");
+
+        portrait.remove();
+        portraitButton.remove();
+    };
+
+    const init = () => {
+        const backgroundButton = document.querySelector(".js-button");
+        const portraitButton = document.querySelector(".js-portraitRemove");
+
+        backgroundButton.addEventListener("click", toggleBackground);
+        portraitButton.addEventListener("click", portraitRemove);
+
+        welcome();
+    };
+
+    init();
 
 
-let section__image = document.querySelector(".section__image");
-let section__portraitRemove = document.querySelector(".section__portraitRemove");
-
-section__portraitRemove.addEventListener("click", () => {
-    section__image.remove();
-    section__portraitRemove.remove();
-});
 
 
-let section__fragmentRemove = document.querySelector(".section__button--fragmentRemove");
-let section__storyFragment = document.querySelector(".section__storyFragment");
 
-section__fragmentRemove.addEventListener("click", () => {
-    section__storyFragment.remove();
-    section__fragmentRemove.remove();
-});
+
+    let section__fragmentRemove = document.querySelector(".section__button--fragmentRemove");
+    let section__storyFragment = document.querySelector(".section__storyFragment");
+
+    section__fragmentRemove.addEventListener("click", () => {
+        section__storyFragment.remove();
+        section__fragmentRemove.remove();
+    });
+};
